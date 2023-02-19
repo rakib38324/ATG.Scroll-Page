83388d7df5c2696353a316687db4d20bb08bead4
rakib38324 <rakib3832@gmail.com>
2023-02-19 21:35:59 +0600

    setup all design and scroll page

---
+++ javaScript.js
@@ -0,0 +1,58 @@
+
+$(document).ready(function () {
+	screenCheck();
+	$('.scroll-control .one').click(function () {
+		$.scrollify.move('#s-one');
+	});
+	$('.scroll-control .two').click(function () {
+		$.scrollify.move('#s-two');
+	});
+	$('.scroll-control .three').click(function () {
+		$.scrollify.move('#s-three');
+	});
+	$('.scroll-control .four').click(function () {
+		$.scrollify.move('#s-four');
+	});
+	$('.scroll-control .five').click(function () {
+		$.scrollify.move('#s-five');
+	});
+	$('.scroll-control .six').click(function () {
+		$.scrollify.move('#s-six');
+	});
+	$('.scroll-control .seven').click(function () {
+		$.scrollify.move('#s-seven');
+	});
+});
+$(window).on('resize', function () {
+	screenCheck();
+});
+function applyScroll() {
+	$.scrollify({
+		section: '.scroll',
+		sectionName: 'section-name',
+		// standardScrollElements: 'section',
+		easing: 'easeOutExpo',
+		scrollSpeed: 0,
+		offset: 0,
+		scrollbars: true,
+		setHeights: true,
+		overflowScroll: true,
+		updateHash: false,
+		touchScroll: true,
+	});
+}
+function screenCheck() {
+	var deviceAgent = navigator.userAgent.toLowerCase();
+	var agentID = deviceAgent.match(/(iphone|ipod|ipad)/);
+	if (agentID || $(window).width() <= 1024) {
+		// its mobile screen
+		$.scrollify.destroy();
+		$('section').removeClass('scroll').removeAttr('style');
+		$.scrollify.disable();
+	} else {
+		// its desktop
+		$('section').addClass('scroll');
+		applyScroll();
+		$.scrollify.enable();
+	}
+}
